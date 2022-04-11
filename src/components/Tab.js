fb191beb4e3dbbff73ecc7e3600c85785ee8d413
kennethzhang <kennethzhang374@gmail.com>
2022-04-10 22:58:34 -0700

    roll back to original design

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,85 +1,35 @@
-import React from "react";
-// import ExpandTab from "./ExpandTab";
+import React, { useState } from "react";
 
-// const Tab = ({ trainings }) => {
-// 	const [isClick, setIsClick] = useState(false);
-// 	const [id, setId] = useState();
-// 	const handleClick = (id) => {
-// 		setIsClick(!isClick);
-// 		setId(id);
-// 		console.log(isClick);
-// 	};
-// 	return (
-// 		<ul className="flex-col c ">
-// 			{trainings.map((training) => (
-// 				<li
-// 					key={training.id}
-// 					className="pb-2 flex-col mx-28 hover:px-2 hover:duration-300 delay-200 font-serif cursor-pointer"
-// 				>
-// 					<div className="ml-4">
-// 						<p
-// 							className="text-3xl font-lg text-gray-900"
-// 							onClick={() => handleClick(training.id)}
-// 						>
-// 							{training.name}
-// 						</p>
-// 					</div>
-// 					<div className="bg-zinc-400 w-full h-1 mb-3"></div>
-// 					{isClick && training.id == id && <ExpandTab training={training} />}
-// 				</li>
-// 			))}
-// 		</ul>
-// 	);
-// };
+import ExpandTab from "./ExpandTab";
 
-const Tab = () => {
+const Tab = ({ trainings }) => {
+	const [isClick, setIsClick] = useState(false);
+	const [id, setId] = useState();
+	const handleClick = (id) => {
+		setIsClick(!isClick);
+		setId(id);
+		console.log(isClick);
+	};
 	return (
-		<div className="accordion" id="accordionExample">
-			<div className="accordion-item bg-white border border-gray-200">
-				<h2 className="accordion-header mb-0" id="headingOne">
-					<button
-						className="
-                                accordion-button
-                                relative
-                                flex
-                                items-center
-                                w-full
-                                py-4
-                                px-5
-                                text-base text-gray-800 text-left
-                                bg-white
-                                border-0
-                                rounded-none
-                                transition
-                                focus:outline-none"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#collapseOne"
-						aria-expanded="true"
-						aria-controls="collapseOne"
-					>
-						Accordion Item #1
-					</button>
-				</h2>
-				<div
-					id="collapseOne"
-					className="accordion-collapse collapse show"
-					aria-labelledby="headingOne"
-					data-bs-parent="#accordionExample"
+		<ul className="flex-col c ">
+			{trainings.map((training) => (
+				<li
+					key={training.id}
+					className="pb-2 flex-col mx-28 hover:px-2 hover:duration-300 delay-200 font-serif cursor-pointer"
 				>
-					<div className="accordion-body py-4 px-5">
-						<strong>This is the first item's accordion body.</strong> It is
-						shown by default, until the collapse plugin adds the appropriate
-						classes that we use to style each element. These classes control the
-						overall appearance, as well as the showing and hiding via CSS
-						transitions. You can modify any of this with custom CSS or
-						overriding our default variables. It's also worth noting that just
-						about any HTML can go within the <code>.accordion-body</code>,
-						though the transition does limit overflow.
+					<div className="ml-4">
+						<p
+							className="text-3xl font-lg text-gray-900"
+							onClick={() => handleClick(training.id)}
+						>
+							{training.name}
+						</p>
 					</div>
-				</div>
-			</div>
-		</div>
+					<div className="bg-zinc-400 w-full h-1 mb-3"></div>
+					{isClick && training.id == id && <ExpandTab training={training} />}
+				</li>
+			))}
+		</ul>
 	);
 };
 
