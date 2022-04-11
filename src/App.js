f61fb3de8afd1b8f04cf00940adb1107774ffad5
kennethzhang <kennethzhang374@gmail.com>
2022-04-10 17:59:30 -0700

    create simple homepage layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
 import "./App.css";
+import React, { useState } from "react";
 
-export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const trainings = [
+	{
+		id: 1,
+		name: "Assembly Basics",
+		instructor: "James Hsu",
+	},
+	{
+		id: 2,
+		name: "Soldering Basics",
+		instructor: "Kenneth Zhang",
+	},
+	{
+		id: 3,
+		name: "Optical Cleaning",
+		instructor: "Nick Reddy",
+	},
+];
+
+const App = () => {
+	return (
+		<div className="m-10">
+			<div className="text-4xl text-center text-blue-900 font-bold font-serif pb-10">
+				Production Training Sign Up Page
+			</div>
+			<ul className="flex-col ">
+				{trainings.map((training) => (
+					<li
+						key={training.id}
+						className="pb-2 flex-col mx-28 hover:px-2 hover:duration-300 delay-200  font-serif"
+					>
+						<div className="ml-4">
+							<p className="text-3xl font-lg text-gray-900">{training.name}</p>
+						</div>
+						<div className="bg-zinc-400 w-full h-1 mb-3"></div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default App;
